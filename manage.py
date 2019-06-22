30fc524ccb05c59e3e30b5b7ebf375b64f1c3d62
ToMMaaY <thuynh629@gmail.com>
2019-06-21 20:33:54 -0700

    initial commit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
